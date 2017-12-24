3757991ff84cf82ce37cf7408b1170ff27c62172
James K <james@gtoet.com>
2017-12-24 17:23:18 +1300

    Removed react-select, will build custom

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,9 +2,6 @@ import React, { Component, PropTypes } from "react";
 import { connect } from "react-redux";
 import classnames from "classnames";
 
-import Select from "react-select";
-import "react-select/dist/react-select.css";
-
 import styles from "./App.css";
 import { increment } from "../actions/counter";
 import { searchQueryUpdated } from "../actions";
@@ -19,42 +16,13 @@ class App extends Component {
   };
 
   onSearchKeyUp = evt => {
-    console.log("styles:");
-    console.log(styles);
-    console.log(`key up recieved event target value was ${evt.target.value}`);
-    console.log(evt);
     this.props.queryChanged(evt.target.value);
-  };
-
-  handleChange = selectedOption => {
-    this.setState({ selectedOption });
-    console.log(`Selected: ${selectedOption.label}`);
   };
 
   render() {
     return (
-      <div
-        className={classnames(styles.customClass, styles.bobo)}
-        style={{ backgroundColor: "blue" }}
-      >
-        <Select
-          value={this.state.selectedOption.value}
-          onChange={this.handleChange}
-          options={[
-            { value: "one", label: "One" },
-            { value: "two", label: "Two" }
-          ]}
-        />
-        I am a react App! Counter is at [{this.props.counter}]<button
-          onClick={this.props.increment}
-        >
-          add one
-        </button>
-        <input
-          onKeyUp={this.onSearchKeyUp}
-          placeholder="Enter search here"
-          onKeyDown={() => console.log("key down")}
-        />
+      <div>
+        <input onChange={this.onSearchKeyUp} placeholder="Enter search here" />
         <select
           style={{ maxHeight: "300px", overflowY: "scroll" }}
           size={15}
@@ -79,9 +47,6 @@ App.propTypes = {
 
 function mapDispatchToProps(dispatch) {
   return {
-    increment: () => {
-      dispatch(increment());
-    },
     queryChanged: q => {
       dispatch(searchQueryUpdated(q));
     }
@@ -90,7 +55,6 @@ function mapDispatchToProps(dispatch) {
 
 function mapStateToProps(state) {
   return {
-    counter: state.counter,
     query: state.query,
     results: state.results
   };
